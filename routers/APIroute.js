dff04628de3722a51ac10ddfa33664071efc24d4
ebharathi <elayabharathi10102002@gmail.com>
2023-10-18 23:20:01 +0530

    added insert command

--- routers/APIroute.js
+++ routers/APIroute.js
@@ -4,7 +4,7 @@ const {executeQuery}=require('../connectDB');
 
 router.post('/create-table',(req,res)=>{
     console.log("[+]CALLED CREATE TABLE COMMAND");
-    let queryToInsert=`CREATE TABLE "${req.body.table}" (
+    let text=`CREATE TABLE "${req.body.table}" (
         "id" SERIAL,
         "name" VARCHAR(100) NOT NULL,
         "role" VARCHAR(15) NOT NULL,
@@ -12,7 +12,7 @@ router.post('/create-table',(req,res)=>{
     );`;
     
     try {
-        executeQuery(queryToInsert).then((result)=>{
+        executeQuery(text).then((result)=>{
             if(result.error==false)
             {
                 console.log(`${req.body.table.toUpperCase()} TABLE CREATED[+]`);
@@ -31,6 +31,38 @@ router.post('/create-table',(req,res)=>{
                 })
             }
         })
+    } catch (error) {
+          console.log("[-]ERROR IN CREATING A TABLE");
+          res.json({
+            error:true,
+            message:error.message
+          })
+    }
+})
+router.post('/insert',(req,res)=>{
+    try {
+        console.log("[+]INSERT COMMAND TRIGGERED");
+        let query=req.body.query;
+        executeQuery(query).then((result)=>{
+            if(result.error==false)
+            {
+                console.log("INSERT COMMAND EXECUTED[+]")
+                res.json({
+                    error:false,
+                    message:result.message
+                })
+            }
+            else
+            {
+                console.log("INSERT COMMAND FAILED[+]");
+                console.log("ERROR: ",result.message);
+                res.json({
+                    error:true,
+                    message:result.message
+                })
+            }
+        })
+
     } catch (error) {
           console.log("[-]ERROR IN CREATING A TABLE");
           res.json({
